d67aeb76d0b372588590e6af1c898ca52078e62c
iindryt <indreskomantaite7@gmail.com>
2025-12-23 14:40:41 +0200

    Add files via upload

---
+++ Mylib.h
@@ -0,0 +1,37 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <ostream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <cctype>
+#include <regex>
+#include <algorithm>
+#include <locale>
+#include <codecvt>
+#include <cwctype>
+
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::setw;
+using std::left;
+using std::ifstream;
+using std::ofstream;
+using std::ostream;
+using std::map;
+using std::set;
+using std::pair;
+using std::vector;
+using std::regex;
+
+#endif
